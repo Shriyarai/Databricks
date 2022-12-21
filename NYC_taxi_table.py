12c34e51caeac18e9bec08f41e4f9fad5df5217c
Shriyarai <Shriyarai>
2022-12-21 04:52:42 +0000

    spark session exp 15

--- NYC_taxi_table.py
+++ NYC_taxi_table.py
@@ -12,7 +12,6 @@
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 #from pyspark.sql.session import SparkSession
-
 #sc = SparkContext.getOrCreate()
 #spark = SparkSession(sc)
 
