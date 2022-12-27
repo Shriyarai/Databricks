47b30858527cd925e85b1e6e803432414b04f4df
Shriyarai <Shriyarai>
2022-12-27 18:59:23 +0000

    test run 9

--- databricks-notebooks/enable_serving.sh
+++ databricks-notebooks/enable_serving.sh
@@ -1,4 +1,4 @@
-POST https://adb-5104278588755913.13.azuredatabricks.net/api/2.0/preview/mlflow/endpoints-v2/enable
+curl -X POST https://adb-5104278588755913.13.azuredatabricks.net/api/2.0/preview/mlflow/endpoints-v2/enable
 
 {
     "registered_model_name": "NYC Taxi Amount API 1"
