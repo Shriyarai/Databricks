0622c9df0c7a512ab9df6ca2687f6e332ee4f8e4
Shriyarai <Shriyarai>
2022-12-27 19:14:02 +0000

    test run 13 :"(

--- databricks-notebooks/enable_serving.sh
+++ databricks-notebooks/enable_serving.sh
@@ -1,5 +1,4 @@
-curl -X POST -u token:$DATABRICKS_TOKEN https://adb-5104278588755913.13.azuredatabricks.net/api/2.0/preview/mlflow/endpoints-v2/enable
-
+curl -X POST -u token:$DATABRICKS_TOKEN https://adb-5104278588755913.13.azuredatabricks.net/api/2.0/preview/mlflow/endpoints-v2/enable \
 {
     "registered_model_name": "NYC Taxi Amount API 1"
 }
