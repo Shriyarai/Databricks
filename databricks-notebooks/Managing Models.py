4f9c9f7646e7a784be4a6c4e68dfd60bc8ab1ca3
Shriyarai <Shriyarai>
2022-12-28 20:57:20 +0000

    fix 2

--- databricks-notebooks/Managing Models.py
+++ databricks-notebooks/Managing Models.py
@@ -537,7 +537,7 @@ forecast_nyc_taxi_amount(model_name, "Production", df)
 # COMMAND ----------
 
 for model_version_info in model_version_infos:
-    if model_version_info.version != new_model_version and model_version_info.stage != "Archived":
+    if model_version_info.version != new_model_version and model_version_info.current_stage != "Archived":
         client.transition_model_version_stage(
             name=model_name,
             version=model_version_info.version,
