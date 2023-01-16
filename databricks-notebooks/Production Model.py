e7241c5492065d7cb38c8a318f4a623a1806f3bc
Shriyarai <Shriyarai>
2023-01-16 09:47:20 +0000

    testing changes

--- databricks-notebooks/Production Model.py
+++ databricks-notebooks/Production Model.py
@@ -13,9 +13,26 @@ client = MlflowClient()
 # COMMAND ----------
 
 model_name = "NYC_Taxi_Amount_API"
+model_stage = "Production"
+
+model_uri = "models:/{model_name}/{model_stage}".format(model_name=model_name,model_stage=model_stage)
+print("Loading registered model version from URI: '{model_uri}'".format(model_uri=model_uri))
+model = mlflow.pyfunc.load_model(model_uri)
+print(model)
+
+# COMMAND ----------
+
+model_name = "NYC_Taxi_Amount_API"
 
 prod_model_info = client.get_latest_versions(model_name, stages=["Production"])
 print(prod_model_info)
+# print(prod_model_info[0].run_id)
+prod_run = mlflow.get_run(run_id=prod_model_info[0].run_id)
+print(prod_run.data.metrics['r2'])
+
+# COMMAND ----------
+
+print(client.get_metric_history(run_id=prod_model_info[0].run_id, key='rmse'))
 
 # COMMAND ----------
 
@@ -24,6 +41,7 @@ model_name = "NYC_Taxi_Amount_API"
 model_version_infos = client.search_model_versions("name = '%s'" % model_name)
 print(model_version_infos)
 new_model_version = max([model_version_info.version for model_version_info in model_version_infos])
+print(new_model_version)
 
 # COMMAND ----------
 
